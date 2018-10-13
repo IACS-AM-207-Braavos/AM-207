d04f040374e9cd0beff88c6fed6fa705211c3d5d
Michael S. Emanuel <michael.s.emanuel@gmail.com>
2018-10-12 21:41:13 -0400

    completed question 3 except 3.6

--- hw5/maximum_uniformity.py
+++ hw5/maximum_uniformity.py
@@ -77,6 +77,8 @@ plt.show()
 sample_size = 100000
 # Given value of theta
 theta = 100
+# Set the random seed
+np.random.seed(42)
 # Build samples with numpy
 x_samples = np.random.uniform(low=0.0, high=theta, size=sample_size)
 # The empirical estimate is just the maximum
@@ -106,21 +108,49 @@ plt.show()
 # 3.6. Make an argument that we can construct a bootstrap confidence interval that 
 # always mismatches the limiting distribution.
 
+# See notebook
 
 # *************************************************************************************************
 # 3.7. Let's switch to being Bayesian. In 3.1 we came up with an expression for the likelihood 
 # fY(y | n,θ)fY(y | n,θ). Use the Pareto distribution to construct a prior for θ. 
 # What are some reasonable values to use for the scale and shape?
 
+# See notebook
+# theta_m = y (minimum theta is maximum data point seen)
+# alpha = 1 (one pseudo-observation)
+# f(theta) = y / theta^2 on [y, infinity)
 
 # *************************************************************************************************
 # 3.8. Write down an expression for the posterior distribution f_Y(θ | n,y)
 
+# See notebook
+# f_post(theta) = ny / theta^(n+1) on [y, infinity)
 
 # *************************************************************************************************
 # 3.9. Draw 10000 posterior samples and plot a histogram of the posterior distribution. 
 # Use percentiles to construct the 68% HPD. Plot the posterior distribution and mark the HPD on your plot.
 
+# Number of samples
+n: int = len(x_samples)
+# Draw 10,000 samples from the posterior, a pareto with shape=n+1 and scale = theta_hat
+# As per the numpy documentation, the library function is for the modified Pareto distribution
+# We need to add 1 and multiply it by x_m to recover the Pareto distribution we want
+theta_samples = theta_hat_ml * (1+np.random.pareto(a=n+1, size=10000))
+# The 68% range spans percentiles 16 to 84
+theta_lo, theta_hi = np.percentile(theta_samples, q=[16, 84])
+# Plot the posterior distribution with the HPD marked
+fig, ax = plt.subplots()
+fig.set_size_inches([16, 8])
+ax.set_title(r'Poseterior Samples of $\theta$')
+ax.set_xlabel(r'$\theta$')
+ax.set_ylabel('Probability')
+# Turn off axis shifting so we can see a regular scale
+ax.get_xaxis().get_major_formatter().set_useOffset(False)
+ax.hist(theta_samples, bins=100, density=True)
+ax.axvline(theta_lo, color='r')
+ax.axvline(theta_hi, color='r')
+ax.grid()
+plt.show()
 
 # *************************************************************************************************
 # 3.10. How does the 68% HPD compare with the confidence interval generated from bootstrapping? 
