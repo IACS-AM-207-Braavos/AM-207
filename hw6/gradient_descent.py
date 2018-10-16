b58a63b50fd8ed4d3ae7ab480292cb78284f8458
Michael S. Emanuel <michael.s.emanuel@gmail.com>
2018-10-16 00:18:46 -0400

    HW6 finished 2.2

--- hw6/gradient_descent.py
+++ hw6/gradient_descent.py
@@ -23,7 +23,10 @@ Mon Oct 15 22:30:43 2018
 # Your goal for this problem is to find the parameters λ1, λ2 that minimize the net loss over the entire dataset.
 
 import numpy as np
+from numpy import exp
 import pandas as pd
+import matplotlib.pyplot as plt
+from am207_utils import range_inc
 
 
 # *************************************************************************************************
@@ -33,7 +36,102 @@ import pandas as pd
 
 # Load the data
 df = pd.read_csv('HW6_data.csv')
+# Extract x1 and x2 
+x1 = df['X_1'].values
+x2 = df['X_2'].values
+X_data = np.vstack([x1, x2]).T
 
+
+def loss_func_abc(xs: np.ndarray, lambdas: np.ndarray, A: float, B: float, C: float) -> float:
+    """
+    General form of the loss function given in this problem.
+    xs:       array of shape (n, 2)
+    lambdas:  array of shape (2, 1)
+    """
+    # Unpack xs into 2 arrays of shape (n, 1)
+    x1 = xs[:, 0]
+    x2 = xs[:, 1]
+    # Unpack lambdas into 2 scalars
+    lambda1: float
+    lambda2: float
+    lambda1, lambda2 = lambdas
+    # Term 1
+    t1: float = A * (lambda2**2) * x2
+    # Term 2
+    t2: float = B * (lambda1**2) * x1
+
+    # Difference of square x2^2 - x1^2
+    ds: float = x2**2 - x1**2
+    # Sum of squares lambda_1^2 + lambda_2^2
+    ss: float = lambda1**2 + lambda2**2
+    # Term 3
+    t3: float = C * lambda1 * x1 * exp(ds * ss)
+    # The function is the sum of these terms
+    return np.sum(t1 + t2 + t3)
+
+# The three numerical constants in the loss function
+A: float = 0.000045
+B: float = -0.000098
+C: float = 0.003926
+
+
+def loss_func(xs: np.ndarray, lambdas: np.ndarray) -> float:
+    """
+    Loss function given in this problem.  Binds parameters A, B, C to their given values.
+    xs:       array of shape (n, 2)
+    lambdas:  array of shape (2, 1)
+    """
+    # Reference to A, B, C and return loss function
+    global A, B, C
+    return loss_func_abc(xs, lambdas, A, B, C)
+
+
+def L(lambda1: float, lambda2: float) -> float:
+    """
+    Loss function in terms of the parameters only; binds A, B, C and xs
+    lambdas:  array of shape (2, 1)
+    """
+    # Reference to X_data
+    global X_data
+    # Combind lambdas into an array
+    lambdas: np.ndarray = np.array([lambda1, lambda2])
+    # Reference data and return loss
+    return loss_func(X_data, lambdas)
+
+
+# *************************************************************************************************
+# 2.1 Visualize the loss function
+
+# Minimum is given
+lambda1_min = 2.05384
+lambda2_min = 0.0
+# lambdas_min = np.array([lambda1_min, lambda2_min])
+loss_min = L(lambda1_min, lambda2_min)
+
+# Grid of lambda1 and lambda2
+grid_size: int = 200
+lambda1_samp = np.linspace(-10, 10, grid_size)
+lambda2_samp = np.linspace(-10, 10, grid_size)
+lambda1_grid, lambda2_grid = np.meshgrid(lambda1_samp, lambda2_samp)
+# Grid of the total loss function
+if 'loss_grid' not in globals():
+    loss_grid = np.zeros((grid_size, grid_size))
+    for i, lambda1 in enumerate(lambda1_samp):
+        for j, lambda2 in enumerate(lambda2_samp):
+            loss_grid[i,j] = L(lambda1, lambda2)
+
+# Plot the loss function
+fig, ax = plt.subplots()
+fig.set_size_inches([16, 8])
+ax.set_title('Loss Function on Entire Data Set')
+ax.set_xlabel(r'$\lambda_1$')
+ax.set_ylabel(r'$\lambda_2$')
+cs = ax.contour(lambda1_grid, lambda2_grid, loss_grid, linewidths=8)
+ax.plot(lambda1_min, lambda2_min, label='Min', marker='o', markersize=12, linewidth=0, color='r')
+fig.colorbar(cs, ax=ax)
+ax.legend()
+ax.grid()
+plt.show()
 
 
 # *************************************************************************************************
@@ -42,7 +140,7 @@ df = pd.read_csv('HW6_data.csv')
 # Your implementation should be stored in a function named gradient_descent.  
 # gradient_descent should take the following parameters (n represents the number of data points):
 
-def gradient_descent():
+def gradient_descent(lambda_init, X_data, step_size, max_iterations, precision, loss):
     """
     Gradient Descent algorithm specialized for this problem.
 
@@ -69,7 +167,59 @@ def gradient_descent():
     'history' -- the associated value is a numpy array with shape (n_iterations,) containing a 
                  history of the calculated value of the loss function at each iteration        
     """
-    pass
+    # Initialize lambdas to lambda_init
+    lambdas = lambda_init
+    # Initialize history to have size max_iterations
+    history = np.zeros(max_iterations+1)
+    # Set shift size h for numerical derivatives of lambda1 and lambda2; use sqrt(machine_epsilon)
+    h: float = 2**-26
+    two_h: float = 2*h
+    # Vectorized shifts to lambdas
+    h_lam1 = np.array([h, 0])
+    h_lam2 = np.array([0, h])
+    # Initialize loss_prev to the loss on the initial parameter values
+    loss_prev: float = loss(X_data, lambdas)
+    history[0] = loss_prev
+    # Perform up to max_iterations steps of gradient descent
+    for i in range_inc(max_iterations):
+        # Compute partial of loss w.r.t lambda1 and lambda2
+        dL_dlam1 = (loss_func(X_data, lambdas + h_lam1) - loss_func(X_data, lambdas - h_lam1)) / two_h
+        dL_dlam2 = (loss_func(X_data, lambdas + h_lam2) - loss_func(X_data, lambdas - h_lam2)) / two_h
+        # Vector gradient dL_dlam
+        grad = np.array([dL_dlam1, dL_dlam2])
+        # Subtract a multiple of the gradient from lambdas
+        lambdas = lambdas - step_size * grad
+        # Compute the current loss
+        loss_curr = loss(X_data, lambdas)
+        # Save the current loss in the history
+        history[i] = loss_curr
+        # Compute the change in the loss function 
+        loss_change = loss_prev - loss_curr
+        # Update loss_prev
+        loss_prev: float = loss_curr
+        # Was the improvement below the precision? Then we can terminate
+        if loss_change < precision:
+            break
+    # Prune history to the number of steps taken
+    history = history[0:i+1]
+    # Create the answer dictionary and return it
+    gd = {'lambdas': lambdas, 
+          'history' : history}
+    return gd
+
+# Run the gradient descent starting at [0, 0] with a learning rate of 0.01
+lambda_init = np.array([0.0, 0.0])
+step_size = 0.01
+max_iterations=10000
+precision=1e-6
+gd = gradient_descent(lambda_init, X_data, step_size, max_iterations, precision, loss_func)
+# Unpack answer
+lambdas = gd['lambdas']
+history = gd['history']
+lambda1, lambda2 = lambdas
+num_iters = len(history)-1
+print(f'Completed gradient descent to precision {precision:0.2e} in {num_iters} steps.')
+print(f'lambda1 = {lambda1:0.6f}, lambda2 = {lambda2:0.6f}')
 
 
 # *************************************************************************************************
