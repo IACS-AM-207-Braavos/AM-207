c029f6eaaa81baf6cfca45e21218d397e10b9808
Michael S. Emanuel <michael.s.emanuel@gmail.com>
2018-10-17 18:46:37 -0400

    HW6 / completed coding of problem 2

--- hw6/am207_utils.py
+++ hw6/am207_utils.py
@@ -8,7 +8,12 @@ Fri Sep 21 17:16:18 2018
 
 import numpy as np
 import matplotlib as mpl
-from typing import Callable
+import pickle
+from typing import Dict, Callable
+
+# *************************************************************************************************
+# Type aliases
+funcType = Callable[[float], float]
 
 # *************************************************************************************************
 def plot_style() -> None:
@@ -45,5 +50,17 @@ def arange_inc(x: float, y: float = None, z: float = None) -> np.ndarray:
 
 
 # *************************************************************************************************
-# Type aliases
-funcType = Callable[[float], float]
+def load_vartbl(fname: str) -> Dict:
+    """Load a dictionary of variables from a pickled file"""
+    try:
+        with open(fname, 'rb') as fh:
+            vartbl = pickle.load(fh)
+    except:
+        vartbl = dict()
+    return vartbl
+
+
+def save_vartbl(vartbl: Dict, fname: str) -> None:
+    """Save a dictionary of variables to the given file with pickle"""
+    with open(fname, 'wb') as fh:
+        pickle.dump(vartbl, fh)
--- hw6/gradient_descent.py
+++ hw6/gradient_descent.py
@@ -27,10 +27,8 @@ from numpy import exp
 import pandas as pd
 import matplotlib.pyplot as plt
 from time import time
-import pickle
-import os
-from am207_utils import range_inc, arange_inc, plot_style
-from typing import List
+from am207_utils import range_inc, arange_inc, plot_style, load_vartbl, save_vartbl
+from typing import List, Dict
 
 # Set plot style
 plot_style()
@@ -46,6 +44,10 @@ df = pd.read_csv('HW6_data.csv')
 x1 = df['X_1'].values
 x2 = df['X_2'].values
 X_data = np.vstack([x1, x2]).T
+
+# Load persisted table of variables
+fname: str = 'gradient_descent.pickle'
+vartbl: Dict = load_vartbl(fname)
 
 
 def loss_func_abc(xs: np.ndarray, lambdas: np.ndarray, A: float, B: float, C: float) -> float:
@@ -111,7 +113,7 @@ def L(lambda1: float, lambda2: float) -> float:
 # Minimum is given
 lambda1_min = 2.05384
 lambda2_min = 0.0
-# lambdas_min = np.array([lambda1_min, lambda2_min])
+# lambda_min = np.array([lambda1_min, lambda2_min])
 loss_min = L(lambda1_min, lambda2_min)
 
 # Grid of lambda1 and lambda2
@@ -121,17 +123,15 @@ lambda2_samp = np.linspace(-10, 10, grid_size)
 lambda1_grid, lambda2_grid = np.meshgrid(lambda1_samp, lambda2_samp)
 
 # Grid of the total loss function
-fname = 'dg_loss_grid.pickle'
-try:
-    with open(fname, 'rb') as fh:
-        loss_grid = pickle.load(fh)
+try:    
+    loss_grid = vartbl['loss_grid']
 except:    
     loss_grid = np.zeros((grid_size, grid_size))
     for i, lambda1 in enumerate(lambda1_samp):
         for j, lambda2 in enumerate(lambda2_samp):
             loss_grid[i,j] = L(lambda1, lambda2)
-    with open(fname, 'wb') as fh:
-        pickle.dump(loss_grid, fh)
+    vartbl['loss_grid'] = loss_grid
+    save_vartbl(vartbl, fname)
 
 # Plot the loss function
 fig, ax = plt.subplots()
@@ -254,7 +254,9 @@ fig.set_size_inches([16, 8])
 ax.set_title('Loss Function During Gradient Descent')
 ax.set_xlabel('Iteration Number')
 ax.set_ylabel('Loss Function')
-ax.plot(plot_n_gd[0:100], history_gd[0:100], linewidth=4)
+ax.plot(plot_n_gd[0:100], history_gd[0:100], label='GD', linewidth=4)
+ax.axhline(y=loss_min, color='r', label='Min')
+ax.legend()
 ax.grid()
 plt.show()
 
@@ -355,19 +357,17 @@ max_iterations=300
 precision=1e-6
 
 # Load the sgd if present, otherwise compute it
-fname = 'gd_sgd.pickle'
-with open(fname, 'rb') as fh:
-    try:
-        sgd = pickle.load(fh)
-        elapsed_sgd = pickle.load(fh)
-    except:
-        t0 = time()
-        sgd = stochastic_gradient_descent(lambda_init, X_data, step_size, scale, max_iterations, precision, loss_func)
-        t1 = time()
-        elapsed_sgd = t1 - t0
-        with open(fname, 'wb') as fh:
-            pickle.dump(sgd, fh)
-            pickle.dump(elapsed_sgd, fh)
+try:
+    sgd = vartbl['sgd']    
+    elapsed_sgd = vartbl['elapsed_sgd']
+    save_vartbl(vartbl, fname)
+except:
+    t0 = time()
+    sgd = stochastic_gradient_descent(lambda_init, X_data, step_size, scale, max_iterations, precision, loss_func)
+    t1 = time()
+    elapsed_sgd = t1 - t0
+    vartbl['sgd'] = sgd
+    vartbl['elapsed_sgd'] = elapsed_sgd
         
 # Unpack answer
 lambdas_sgd = sgd['lambdas']
@@ -375,7 +375,7 @@ history_sgd = sgd['history']
 lambda1_sgd, lambda2_sgd = lambdas_sgd
 num_iters_sgd = len(history_sgd)-1
 print(f'Completed stochastic gradient descent to precision {precision:0.2e} in {num_iters_sgd} steps.')
-print(f'lambda1 = {lambda1:0.6f}, lambda2 = {lambda2:0.6f}')
+print(f'lambda1 = {lambda1_sgd:0.6f}, lambda2 = {lambda2_sgd:0.6f}')
 print(f'Elapsed time {elapsed_sgd:0.2f} seconds, {elapsed_sgd/num_iters_gd:0.2e} per second.')
 
 
@@ -389,7 +389,9 @@ fig.set_size_inches([16, 8])
 ax.set_title('Loss Function During Stochastic Gradient Descent')
 ax.set_xlabel('Iteration Number')
 ax.set_ylabel('Loss Function')
-ax.plot(plot_n_sgd[0:100], history_sgd[0:100], linewidth=4)
+ax.plot(plot_n_sgd[0:100], history_sgd[0:100], label='SGD', linewidth=4)
+ax.axhline(y=loss_min, color='r', label='Min')
+ax.legend()
 ax.grid()
 plt.show()
 
@@ -409,15 +411,13 @@ plt.show()
 precision_3dp = 1e-3
 gd_3dp = gradient_descent(lambda_init, X_data, step_size, scale, max_iterations, precision_3dp, loss_func)
 # Load gradient descent calculations to 3 decimal places if present, otherwise compute it
-fname = 'gd_gd_3dp.pickle'
 try:
-    with open(fname, 'rb') as fh:
-        std_3dp = pickle.load(fh)
+    sgd_3dp = vartbl['sgd_3dp']
 except:
     sgd_3dp = stochastic_gradient_descent(lambda_init, X_data, step_size, scale, 
                                           max_iterations, precision_3dp, loss_func)
-    with open(fname, 'wb') as fh:
-        pickle.dump(sgd_3dp, fh)
+    vartbl['sgd_3dp'] = sgd_3dp
+    save_vartbl(vartbl, 'sgd_3dp')
 # Report results
 num_iters_gd_3dp = len(gd['history'])
 num_iters_sgd_3dp = len(sgd['history'])
@@ -439,12 +439,10 @@ lambda_init = np.array([0, 0])
 # Set max_iterations for this experiment at 50 so runs don't take too long
 max_iterations_sgd = 50
 # Load SGD by learning rate if present, otherwise compute it
-fname = 'gd_by_lr.pickle'
 try:
-    with open(fname, 'rb') as fh:
-        sgds_by_lr = pickle.load(fh)
-        num_iters_by_lr = pickle.load(fh)
-        loss_by_lr = pickle.load(fh)
+    sgds_by_lr = vartbl['sgds_by_lr']
+    num_iters_by_lr = vartbl['num_iters_by_lr']
+    loss_by_lr = vartbl['loss_by_lr']
 except:       
     # List of sgd objects and iteration counts
     sgds_by_lr: List[dict] = num_step_sizes * [dict()]
@@ -457,10 +455,10 @@ except:
         sgds_by_lr[i] = sgd_curr
         num_iters_by_lr[i] = len(sgd_curr['history'])-1
         loss_by_lr[i] = sgd_curr['history'][-1]
-    with open(fname, 'wb') as fh:
-        pickle.dump(sgds_by_lr, fh)
-        pickle.dump(num_iters_by_lr, fh)
-        pickle.dump(loss_by_lr, fh)
+    vartbl['sgds_by_lr'] = sgds_by_lr
+    vartbl['num_iters_by_lr'] = num_iters_by_lr
+    vartbl['loss_by_lr'] = loss_by_lr
+    save_vartbl(vartbl, fname)
 
 # Plot number of iterations vs. learning rate
 fig, ax = plt.subplots()
@@ -469,7 +467,7 @@ ax.set_title('Number of Iterations vs. Learning Rate in SGD')
 ax.set_xlabel('Step Size (Learning Rate), log scale')
 ax.set_ylabel('Number of Iterations for 0.001 Loss Tolerance')
 ax.set_xscale('log')
-ax.plot(step_sizes, num_iters_by_lr, marker='o')
+ax.plot(step_sizes, num_iters_by_lr, label='SGD', marker='o')
 ax.grid()
 plt.show()
 
@@ -480,7 +478,9 @@ ax.set_title('Loss After 50 Iterations vs. Learning Rate in SGD')
 ax.set_xlabel('Step Size (Learning Rate), log scale')
 ax.set_ylabel('Loss After 50 Iterations')
 ax.set_xscale('log')
-ax.plot(step_sizes, loss_by_lr, marker='o')
+ax.plot(step_sizes, loss_by_lr, label='SGD', marker='o')
+ax.axhline(y=loss_min, color='r', label='Min')
+ax.legend()
 ax.grid()
 plt.show()
 
@@ -503,11 +503,9 @@ max_iterations = 100
 # Set a very tight precision so we won't exit early
 precision_tight = 1e-16
 # Test whether this has been done on a prior run in this session because it's slow
-fname = 'gd_by_start'
 try:
-    with open(fname, 'rb') as fh:
-        gds_by_start = pickle.load(fh)
-        sgds_by_start = pickle.load(fh)
+    gds_by_start = vartbl['gds_by_start']
+    sgds_by_start = vartbl['sgds_by_start']                           
 except:
     # Create lists to store the results
     gds_by_start = num_starts * [dict()]
@@ -522,9 +520,10 @@ except:
         # Save the two models
         gds_by_start[i] = gd_curr
         sgds_by_start[i] = sgd_curr
-    with open(fname, 'wb') as fh:
-        pickle.dump(gds_by_start, fh)
-        pickle.dump(sgds_by_start, fh)
+    # Save these calculations
+    vartbl['gds_by_start'] = gds_by_start
+    vartbl['sgds_by_start'] = sgds_by_start
+    save_vartbl(vartbl, fname)
             
 
 # Loss function vs. iteration iteration for each starting value
@@ -536,18 +535,26 @@ for i, lambda_init in enumerate(lambdas_init):
     loss_sgd_by_start[i] = sgds_by_start[i]['history']
 
 # Plot loss vs. iteration for each start
-nn = arange_inc(1, max_iterations)
-for lambda_init in lambdas_init:
+nn = arange_inc(0, max_iterations)
+for i, lambda_init in enumerate(lambdas_init):
     lambda1, lambda2 = lambda_init
     fig, ax = plt.subplots()
-    ax.set_title(f'Loss vs. Iteration Number Starting at $\lambda_1=${lambda1:0.3f}, $\lambda_2={lambda2:0.1f}')
+    fig.set_size_inches([16,8])
+    ax.set_title(f'Loss vs. Iteration Number Starting at $\lambda_1={lambda1:0.3f}$, $\lambda_2={lambda2:0.1f}$')
     ax.set_xlabel('Iteration Number')
     ax.set_ylabel('Loss Function')
-    # plot_loss_gd = loss_gd_by_start[i]
-    # plot_loss_sgd = loss_sgd_by_start[i]
-    ax.plot(nn, loss_gd_by_start[i], label='GD')
-    ax.plot(nn, loss_sgd_by_start[i], label='SGD')
+    # Data to plot on this iteration - loss by iteration for GD & SGD
+    plot_loss_gd = loss_gd_by_start[i]
+    plot_loss_sgd = loss_sgd_by_start[i]
+    n_gd = plot_loss_gd.shape[0]
+    n_sgd = plot_loss_sgd.shape[0]
+    # Plot the loss by iteration
+    ax.plot(nn[0:n_gd], plot_loss_gd, label='GD')
+    ax.plot(nn[0:n_sgd], plot_loss_sgd, label='SGD')
+    ax.axhline(y=loss_min, color='r', label='Min')
     ax.legend()
     ax.grid()
     plt.show()
     
+# Save all persisted variable
+save_vartbl(vartbl, fname)
